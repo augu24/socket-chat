f737aa6bf199dee33c91eb96fc0291351595f723
Augustin <71448991+augu24@users.noreply.github.com>
2023-05-17 00:46:14 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
 from socket import AF_INET, socket, SOCK_STREAM
 from threading import Thread
 
+import sys
+import errno
+
 global d
 d = 11
 
@@ -27,14 +30,14 @@ def decrypter(texte_chiffre, decalage):
     return resultat
 
 def manageClient(client): 
-    username = client.recv(1024).decode("utf8")
+    username = client.recv(4096).decode()
     username = decrypter(username, d)
-    client.send(bytes(encrypter(f"{username} vient de rejoindre le serveur."),d), "utf8")
+    client.send(encrypter(f"{username} vient de rejoindre le serveur.", d).encode())
     repeteur(f"{username} vient de rejoindre.")
     clients[client] = username
 
     while True:
-        message = client.recv(1024)
+        message = client.recv(4096).decode()
         message = decrypter(message, d)
         repeteur(message, username+": ")
 
@@ -43,30 +46,32 @@ def accepteConnexion():
         client, addrClient = server.accept()
         print(f"{addrClient[1]} vient de se connecter.")
         messBienvenue = encrypter("Bienvenue sur Socket Chat! Merci de rentrer votre nom d'utilisateur:", d)
-        client.send(bytes(messBienvenue, "utf8"))
+        client.send(messBienvenue.encode())
         addresses[client] = addrClient
-        Thread(target=manageClient, args=client).start()
+        Thread(target=manageClient, args=(client,)).start() #args=(client,) car args doit etre tuple et (variable,) la virgule marque une tuple de length 1
 
 
 def repeteur(message, prefix=""):
+    message = prefix+message
     message = encrypter(message, d)
-    if prefix != "":
-        avant = encrypter(prefix, d)
-    else:
-        avant=""
-    for soket in clients:
-        soket.send(bytes((avant+message), "utf8"))
+    try:
+        for soket in clients:
+            soket.send(message.encode())
+    except IOError as e:
+        if e.errno == errno.EPIPE:
+            pass
+
 
 clients = {}
 addresses = {}
 
 port = 8080
 host = "127.0.0.1"
-addr = (host, port)
 
 server = socket(AF_INET, SOCK_STREAM)
-server.bind(addr)
+server.bind((host, port))
 server.listen()
+
 print("En attente de connexion ...")
 accepte_thread = Thread(target=accepteConnexion)
 accepte_thread.start()
