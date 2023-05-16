020d4bca55f91a31f2504ea453a0c78884c0b035
Augustin <71448991+augu24@users.noreply.github.com>
2023-05-17 00:45:44 +0200

    Update client.py

--- client.py
+++ client.py
@@ -1,10 +1,20 @@
 
-from socket import AF_INET, socket, SOCK_STREAM
+from socket import AF_INET, socket, SOCK_STREAM 
+#AF_INET = type d'addresse ici l'IP sera alors une string et le port un entier
+#SOCKET_STREAM = type de socket ici correspond au type TCP
+
 from threading import Thread
 import tkinter
 
 global d
 d = 11
+
+
+def on_enter(event):
+    texte.config(bg='green', fg='black')
+
+def on_leave(event):
+    texte.config(bg='black', fg='green')
 
 def encrypter(texte, decalage):
     resultat = ""
@@ -32,19 +42,23 @@ def envoyer():
     message = monMess.get()
     monMess.set("")
     message = encrypter(message, d)
-    socketClient.send(bytes(message, "utf8"))
+
+    socketClient.send(message.encode())
 
 
 def recevoir():
     while True:
         try:
-            message = socketClient.recv(1024).decode("utf8")
+
+            message = socketClient.recv(4096).decode()
+
             message = decrypter(message, d)
             listeMess.insert(tkinter.END, message)
         except:  
             break
 
 def fermeture():
+    socketClient.send(encrypter("vient de quitter.").encode())
     socketClient.close()
     top.quit()
 
@@ -55,6 +69,8 @@ screen_x = int(top.winfo_screenwidth())
 screen_y = int(top.winfo_screenheight())
 top.geometry(f"700x480+{screen_x// 2 - 350}+{screen_y// 2 - 300}")
 top.configure(bg="black")
+global titre
+titre = tkinter.Label(top, text = "Bienvenue sur pychat !",bg = "Black", fg = "green", font = ("Courier",30))
 top.protocol("WM_DELETE_WINDOW", fermeture)
 
 cadreMess = tkinter.Frame(top)
@@ -63,22 +79,31 @@ monMess.set("")
 scrollbar = tkinter.Scrollbar(cadreMess)
 scrollbar = tkinter.Scrollbar(cadreMess)
 listeMess = tkinter.Listbox(cadreMess, height=15, width=50, yscrollcommand=scrollbar.set,fg="green",  highlightthickness=3, highlightbackground="green", highlightcolor="green")
-listeMess.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
 listeMess.configure(bg="black")
-listeMess.pack()
-cadreMess.pack()
 champs = tkinter.Entry(top, textvariable=monMess,fg = "green", bg = "black",highlightbackground="green",insertbackground="green")
 champs.bind("<Return>", envoyer)
-champs.pack(pady=(10,0))
 texte = tkinter.Label(top, text="Envoyer (⏎)", fg='green',bg = "black")
-texte.pack()
+texte.bind("<Button-1>",lambda event:envoyer())
+texte.bind("<Enter>",on_enter )
+texte.bind("<Leave>",on_leave)
 
 port = 8080
 host = "127.0.0.1" 
+try:
 
-socketClient = socket(AF_INET, SOCK_STREAM)
-socketClient.connect((host, port))
-recevoir_thread = Thread(target=recevoir)
-recevoir_thread.start()
-tkinter.mainloop() 
-## print("Une erreur est survenu, veuilliez reesayer.")
+    socketClient = socket(AF_INET, SOCK_STREAM)
+    socketClient.connect((host, port))
+
+    recevoir_thread = Thread(target=recevoir)
+    recevoir_thread.start()
+
+    titre.pack(pady=(20,10))
+    listeMess.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
+    cadreMess.pack()
+    champs.pack(pady=(10,0))
+    texte.pack()
+    tkinter.mainloop() 
+
+except:
+    print("Une erreur est survenu.")
+
